ef81cec59578b34c46e22226974f4c339757b41e
avril-m-harder <avrilharder@gmail.com>
2021-08-26 08:45:53 -0500

    update

--- 01_slim/scripts/slim_to_consensus_FASTAs.sh
+++ 01_slim/scripts/slim_to_consensus_FASTAs.sh
@@ -2,8 +2,8 @@
 #
 #   +-----------------------+
 #   |  USE:                 |
-#   |    - ##### queue      |
-#   |    - ## CPU + ## Gb   |
+#   |    - SMALL queue      |
+#   |    - 1 CPU + def Gb   |
 #   +-----------------------+
 #
 #  Replace the USER name in this script with your username and
@@ -40,31 +40,33 @@ cd /scratch/${USER}_${PROJ}/
 
 ## --------------------------------
 ## Load modules
+module load bcftools/1.13
 module load anaconda
-module load bcftools
-
 
 ## --------------------------------
 ## Run SLiM
-# mkdir slim_output_files
+mkdir slim_output_files
 
-# slim \
-#   -d NUM_GEN=1000 \
-#   -d MUTATION_RATE=100e-8 \
-#   -d REC_RATE=250e-8 \
-#   -d CHR_LENGTH=30000000 \
-#   -d POP_SIZE=100 \
-#   -d SAMPLE_SIZE=30 \
-#   -d "OUT_PATH='/slim_output_files/'" \
-#   /home/$USER/roh_param_project/$PROJ/scripts/chrom_evo_and_sample.slim
+currentDate=`date`
+echo $currentDate
 
+slim \
+  -d MUTATION_RATE=100e-8 \
+  -d REC_RATE=250e-8 \
+  -d CHR_LENGTH=30000000 \
+  -d POP_SIZE=100 \
+  -d "OUT_PATH='/scratch/${USER}_${PROJ}/slim_output_files/'" \
+  /home/$USER/roh_param_project/$PROJ/scripts/chrom_evo_and_sample.slim
+
+currentDate=`date`
+echo $currentDate
 
 ## --------------------------------
 ## Format SLiM output for read simulation
 cd slim_output_files/
 
 ## Compress output VCF
-# bgzip final_pop.vcf
+bgzip final_pop.vcf
 
 ## Index compressed VCF
 tabix final_pop.vcf.gz
@@ -77,17 +79,18 @@ bcftools +split -O z -o sample_vcf_files/ ./slim_output_files/final_pop.vcf.gz
 
 ## Randomly select sample VCF files for conversation to FASTAs
 ## >> Would be more efficient to process 100 and downsample later, but this keeps
-## >> each set of samples an independent draw from the final population
+## >> each set of samples an independent draw from the final population. Can change to 
+## >> downsampling if we need to run multiple replicate simulations later on.
 cd sample_vcf_files
 
 for a in 15 30 50 100
 	do
-	ls i*.vcf.gz | sort -R | tail -100 > ../vcf_file_list_n.$a.txt
+	ls i*.vcf.gz | sort -R | tail -$a > ../vcf_file_list_n.$a.txt
 	sed 's/.vcf.gz//g' ../vcf_file_list_n.$a.txt > ../sample_id_list_n.$a.txt
 	done
 
 
-## Convert sample VCF files to two separate haplotype FASTAs
+## Convert sample VCF files to two separate haplotype FASTAs per individual
 cd ../
 mkdir sample_fasta_files
 cd sample_fasta_files/
@@ -97,16 +100,29 @@ for a in 15 30 50 100
 	while read -a line
 		do
 		tabix ../sample_vcf_files/${line[0]}.vcf.gz
-	
+		
+		bcftools norm --check-ref s \
+		--fasta-ref ../slim_output_files/ancestral.fasta \
+		--multiallelics - \
+		--do-not-normalize \
+		--output ../sample_vcf_files/norm_${line[0]}.vcf \
+		../sample_vcf_files/${line[0]}.vcf.gz
+		
+		bgzip ../sample_vcf_files/norm_${line[0]}.vcf
+		tabix ../sample_vcf_files/norm_${line[0]}.vcf.gz
+
+		bcftools +fixref ../sample_vcf_files/norm_${line[0]}.vcf.gz -- \
+		--fasta-ref ../slim_output_files/ancestral.fasta
+			
 		bcftools consensus --haplotype 1 \
 		--fasta-ref ../slim_output_files/ancestral.fasta \
 		--output ${line[0]}_1_n.$a.fasta \
-		../sample_vcf_files/${line[0]}.vcf.gz
+		../sample_vcf_files/norm_${line[0]}.vcf.gz
 	
 		bcftools consensus --haplotype 2 \
 		--fasta-ref ../slim_output_files/ancestral.fasta \
 		--output ${line[0]}_2_n.$a.fasta \
-		../sample_vcf_files/${line[0]}.vcf.gz
+		../sample_vcf_files/norm_${line[0]}.vcf.gz
 	
 		done < ../sample_id_list_n.$a.txt
 	done
