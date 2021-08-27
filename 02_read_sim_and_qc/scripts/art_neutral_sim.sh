eb352b0f078ceb5a8628947c4bab0beba3379b90
Avril Harder <54415474+avril-m-harder@users.noreply.github.com>
2021-08-27 09:21:25 -0500

    Update art_neutral_sim.sh

--- 02_read_sim_and_qc/scripts/art_neutral_sim.sh
+++ 02_read_sim_and_qc/scripts/art_neutral_sim.sh
@@ -1,3 +1,8 @@
+## Script published alongside:
+## Lou RN, Jacobs A, Wilder A, Therkildsen NO. A beginner's 
+## guide to low-coverage whole genome sequencing for population genomics. 
+## Molecular Ecology. 2021 Jul. DOI: 10.1111/mec.16077.
+
 #!/bin/bash
 REP_ID=$1
 DIR=${2:-/workdir/lcwgs-simulation/neutral_sim/}
