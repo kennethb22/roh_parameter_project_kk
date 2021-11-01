6a570d6fe2566fa15af1e1b3f6c30b4e14df394d
Kenneth Kirksey <kbk0024@auburn.edu>
2021-11-01 17:55:44 -0500

    Add code to create map file and log file to 05a_snp_calling.sh

--- scripts/05_var_call/05a_snp_calling.sh
+++ scripts/05_var_call/05a_snp_calling.sh
@@ -23,31 +23,33 @@
 # -----------------------------------------------------------------------------
 
 ## Load variables from settings file
-source /home/aubkbk001/roh_param_project/init_script_vars.sh
+source /home/aubkbk001/roh_param_project/scripts/99_includes/init_script_vars.sh
 
 ## Set step name
 STEP=05_var_call
 PREV_STEP=04_downsample
+SCRIPT=05a_snp_calling
 
 ## Create variable pointing to user's home directory for this project and step
-HOME_STEP_DIR=/home/${USER}/${PROJECT}/${STEP}/
+HOME_STEP_DIR=/home/${USER}/${PROJECT}/data/${STEP}/
 
 ## Set the input directory, which is the output directory from the previous
 ## step
-INPUT_DIR=/scratch/${USER}/${PROJECT}/${PREV_STEP}/output
+INPUT_DIR=/scratch/${USER}/${PROJECT}/data/${PREV_STEP}/output
 
 ## Create a working directory on /scratch
-mkdir -p /scratch/${USER}/${PROJECT}/${STEP}
+mkdir -p /scratch/${USER}/${PROJECT}/data/${STEP}
 chmod -R 700 /scratch/${USER}
-WORK_DIR=/scratch/${USER}/${PROJECT}/${STEP}
+WORK_DIR=/scratch/${USER}/${PROJECT}/data/${STEP}
 
 ## Create output directory in working directory
 mkdir ${WORK_DIR}/output
 chmod -R 700 ${WORK_DIR}
 OUTPUT_DIR=${WORK_DIR}/output
 
-## --------------------------------
-## Load modules
+# -----------------------------------------------------------------------------
+# Load modules
+# -----------------------------------------------------------------------------
 module load picard/1.79
 module load gatk/4.1.4.0
 
@@ -58,39 +60,130 @@ module load gatk/4.1.4.0
 # Iterate over each combination of population and coverage level. Sets of
 # population and coverage are set in init_script_vars.sh
 
-# Iterate over populations
+# Iterate over populations -----------------------------------------------------
+
 for population in ${popN[@]}; do
 
-    # Iterate over levels of coverage
+    # Iterate over levels of coverage ------------------------------------------
+
     for i in $(seq 0 $cvgCnt); do
+
         # Create output directory for each population size and coverage level
         # combination.
+
         POP_CVG_OUTPUT_DIR=${OUTPUT_DIR}/sample_pop_${population}_cvg_${cvgX[i]}
         mkdir ${POP_CVG_OUTPUT_DIR}
 
         # Set input directory for this population and coverage
+
         POP_CVG_INPUT_DIR=${INPUT_DIR}/sample_pop_${population}_cvg_${cvgX[i]}
 
-        # Process each individual sample file
+        # Set name of map file for this population and coverage. This file
+        # is used in 05b_genomicsdb.sh to import the samples into GenomicsDB
+
+        MAP_FILE=${OUTPUT_DIR}/sample_pop_${population}_cvg_${cvgX[i]}_map.txt
+
+        # Delete map file if it exists
+
+        if [ -f "$MAP_FILE" ]; then
+            rm ${MAP_FILE}
+        fi
+
+        # Set name of log file to track execution times
+
+        LOG_FILE=${OUTPUT_DIR}/${SCRIPT}_sample_pop_${population}_cvg_${cvgX[i]}_log.txt
+
+        # Delete log file if it exists
+        if [ -f "$LOG_FILE" ]; then
+            rm ${LOG_FILE}
+        fi
+
+        # Write header to log file
+
+        printf "%-80s   %8s   %8s   %8s\n" "Action - Output" "Start" "End" "Duration" >>${LOG_FILE}
+
+        # Process each individual sample file ----------------------------------
+
         while read -a line; do
 
-            # ## Add read group information
+            ## Add read group information --------------------------------------
+
+            ## Set action and start time for log
+
+            OUT_FILE=${line[0]}_pop_${population}_cvg_${cvgX[i]}_rgroups.bam
+            ACTION="picard/AddOrReplaceReadGroups - ${OUT_FILE}"
+            START_TIME_HHMM=$(date +" %T")
+            START_TIME=$(date +%s)
+
+            ## Do picard read groups
+
             java -jar /opt/asn/apps/picard_1.79/picard_1.79/picard-tools-1.79/AddOrReplaceReadGroups.jar \
                 I=${POP_CVG_INPUT_DIR}/${line[0]}_pop_${population}_cvg_${cvgX[i]}.bam \
-                O=${POP_CVG_OUTPUT_DIR}/${line[0]}_pop_${population}_cvg_${cvgX[i]}_rgroups.bam \
-                SORT_ORDER=coordinate RGID=group1 RGLB=lib1 RGPL=illumina RGSM=${line[0]} RGPU=unit4
+                O=$${POP_CVG_OUTPUT_DIR}/{OUT_FILE} SORT_ORDER=coordinate RGID=group1 RGLB=lib1 RGPL=illumina RGSM=${line[0]} RGPU=unit4
 
-            # Index BAM files
+            ## Write line to log file for this action
+
+            END_TIME_HHMM=$(date +"%T")
+            END_TIME=$(date +%s)
+            ELAPSED=$(expr $END_TIME - $START_TIME)
+            DURATION=$(date -u -d @${ELAPSED} +"%T")
+            printf '%-80s   %8s   %8s   %8s\n' "${ACTION}" ${START_TIME_HHMM} ${END_TIME_HHMM} ${DURATION} >>${LOG_FILE}
+
+            ## Index BAM files -------------------------------------------------
+
+            ## Set action and start time for log
+
+            OUT_FILE=${line[0]}_pop_${population}_cvg_${cvgX[i]}_rgroups.bai
+            ACTION="picard/BuildBamIndex - ${OUT_FILE}"
+            START_TIME_HHMM=$(date +" %T")
+            START_TIME=$(date +%s)
+
+            ## Run picard BuildBamIndex
+
             java -jar /opt/asn/apps/picard_1.79/picard_1.79/picard-tools-1.79/BuildBamIndex.jar \
                 I=${POP_CVG_OUTPUT_DIR}/${line[0]}_pop_${population}_cvg_${cvgX[i]}_rgroups.bam
 
-            ## Run HaplotypeCaller in GVCF mode
+            ## Write line to log file for this action
+
+            END_TIME_HHMM=$(date +"%T")
+            END_TIME=$(date +%s)
+            ELAPSED=$(expr $END_TIME - $START_TIME)
+            DURATION=$(date -u -d @${ELAPSED} +"%T")
+            printf '%-80s   %8s   %8s   %8s\n' "${ACTION}" ${START_TIME_HHMM} ${END_TIME_HHMM} ${DURATION} >>${LOG_FILE}
+
+            ## Run HaplotypeCaller in GVCF mode --------------------------------
+
+            ## Set action and start time for log
+
+            OUT_FILE=${line[0]}_pop_${population}_cvg_${cvgX[i]}.g.vcf
+            ACTION="gatk Haplotype Caller - ${OUT_FILE}"
+            START_TIME_HHMM=$(date +" %T")
+            START_TIME=$(date +%s)
+
+            ## Run gatk HaplotypeCaller
+
             gatk HaplotypeCaller \
                 -R ${REF_GENOME_FILE} \
                 -I ${POP_CVG_OUTPUT_DIR}/${line[0]}_pop_${population}_cvg_${cvgX[i]}_rgroups.bam \
                 -stand-call-conf 20.0 \
                 --emit-ref-confidence GVCF \
-                -O ${POP_CVG_OUTPUT_DIR}/${line[0]}_pop_${population}_cvg_${cvgX[i]}.g.vcf
+                -O ${POP_CVG_OUTPUT_DIR}/${OUT_FILE}
+
+            ## Write line to log file for this action
+
+            END_TIME_HHMM=$(date +"%T")
+            END_TIME=$(date +%s)
+            ELAPSED=$(expr $END_TIME - $START_TIME)
+            DURATION=$(date -u -d @${ELAPSED} +"%T")
+            printf '%-80s   %8s   %8s   %8s\n' "${ACTION}" ${START_TIME_HHMM} ${END_TIME_HHMM} ${DURATION} >>${LOG_FILE}
+
+            ## Write line for sample to map file.
+
+            printf "%s\t%s\n" \
+                ${line[0]} \
+                ${line[0]}_pop_${population}_cvg_${cvgX[i]}.g.vcf \
+                >>$MAP_FILE
+
         done <${INPUT_DIR}/sample_id_list_pop_${population}.txt
     done
 done
@@ -99,4 +192,4 @@ done
 # Copy output files to user's home directory.
 # -----------------------------------------------------------------------------
 
-source /home/aubkbk001/roh_param_project/backup_output.sh
+source /home/aubkbk001/roh_param_project/scripts/99_includes/backup_output.sh
--- scripts/99_includes/init_script_vars.sh
+++ scripts/99_includes/init_script_vars.sh
@@ -22,6 +22,9 @@ USER_HOME_DIR=/home/${USER}/
 ## Set email address
 EMAIL=kirkseykb1@appstate.edu
 
+## Set initial step name
+STEP=01_slim
+
 # -----------------------------------------------------------------------------
 # These arrays are used in the steps:
 #    04_downsample.sh
@@ -32,9 +35,11 @@ EMAIL=kirkseykb1@appstate.edu
 ## Coverage level in NNx for display in output file names
 # declare -a cvgX=(50x 30x 15x 10x 05x)
 declare -a cvgX=(05x)
+
 ## Coverage level fraction to supply to samtools
 # declare -a cvgP=(1.0 0.6 0.3 0.2 0.1)
 declare -a cvgP=(0.1)
+
 ## Get length of the coverage level arrays. Subtract 1 because arrays are zero
 ## based, and we'll iterate over the arrays from 0 to cvgCnt
 cvgCnt=${#cvgX[@]}
@@ -43,10 +48,11 @@ let cvgCnt-=1
 ## Create array of population sizes we want to test
 # declare -a popN=(100 50 30)
 declare -a popN=(03)
+# declare -a popN=(01)
+
 # -----------------------------------------------------------------------------
-#
 ## Set location of reference genome file
-REF_GENOME_FILE_PATH=/scratch/${USER}_01_slim/slim_output_files_m5e-07_r1e-8_p500
+REF_GENOME_FILE_PATH=/scratch/${USER}/${PROJECT}/data/${STEP}/output/slim_output_files_m5e-07_r1e-8_p500
 
 REF_GENOME_FILE_NAME=ancestral
 
